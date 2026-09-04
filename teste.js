0094c9e7b259e4845cf8718eaf5248ffdd8431b9
Giovanni Prando <giovanni.prando615@gmail.com>
2026-09-04 15:51:57 -0300

    Primeiro commit

---
+++ teste.js
@@ -0,0 +1,128 @@
+/* // Exercício 1
+let idade = 17;
+
+if (idade >= 18) {
+  console.log("Maior de idade");
+} else {
+  console.log("Menor de idade");
+}
+*/
+// Exercício 2
+/* 
+let nota = 8;
+if (nota >= 7) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
+ */
+// Exercício 3
+/* 
+let numero = -5;
+if (numero > 0) {
+  console.log("Positivo");
+} else {
+  console.log("Negativo");
+}
+ */
+// Exercício 4
+/* 
+let numero = 10;
+if (numero % 2 === 0) {
+  console.log("O número é par");
+} else {
+  console.log("O número é impar");
+}
+ */
+// Exercício 5
+/* 
+let nota = 8;
+let frequencia = 80;
+
+if (nota >= 7 && frequencia >= 75) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
+*/
+// Exercício 6
+/* 
+let valorCompra = 400;
+let possuiCartao = true;
+
+if (valorCompra > 500 || possuiCartao) {
+  console.log("Cliente ganhou desconto!");
+} else {
+  console.log("Cliente não ganhou desconto!");
+}
+ */
+// Exercício 7
+/* 
+let usuario = "admin";
+let senha = "1234";
+
+if (usuario === "admin" && senha === "1234") {
+  console.log("Login Realizado!");
+} else {
+  console.log("Usuário ou senha incorretos!");
+}
+ */
+
+// Exercício 8
+/* 
+let idade = 25;
+
+if ((idade === 0) === 12) {
+  console.log("Criança");
+} else if (idade >= 13 && idade <= 17) {
+  console.log("Adolecente");
+} else {
+  console.log("Adulto");
+}
+ */
+
+//Exercício 9
+/* 
+let compra = 250;
+let clienteVIP = true;
+
+if (compra >= 300 && clienteVIP) {
+  console.log("Frete grátis!");
+} else {
+  console.log("Frete pago!");
+}
+ */
+// Atividade 10
+/* 
+let idade = 21;
+let possuiIngresso = true;
+
+if (idade >= 18 && possuiIngresso) {
+  console.log("Liberada");
+} else {
+  console.log("Bloqueada");
+}
+ */
+//Desafio Final
+/* 
+let nome = "Carlos";
+let idade = 18;
+let nota = 8;
+let frequencia = 80;
+console.log("========================");
+console.log(" RESULTADO");
+console.log("========================");
+console.log("");
+console.log("Aluno: " + nome);
+console.log("Idade: " + idade);
+console.log("Nota: " + nota);
+console.log("Frequência: " + frequencia + "%");
+console.log("");
+
+if (nota >= 8 && frequencia >= 75) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+} */
+
+//Desafio Extra
